3fdec978093e8bcb80a5ad7cd8f54b3ca0d9697e
Matías Castillo <matcasti@umag.cl>
2022-06-20 00:21:40 -0300

    more GAM functionalities

--- R/report_stats.R
+++ R/report_stats.R
@@ -2,7 +2,7 @@
 #'
 #' Internal function to round p-values
 #'
-#' @param x Numeric. P-value to be formated.
+#' @param x Numeric. p-value to be formatted.
 
 format_p <- function(x) {
   stopifnot(
@@ -78,7 +78,7 @@ report_chi <- function(chi, effsize = TRUE, ci = 0.95) {
 #'
 #' @export
 
-report_slope <- function(model, ..., ci = 0.95) {
+report_overall <- function(model, ..., ci = 0.95) {
 
   stopifnot(
     "Is not a GAM model" = inherits(model, "gam")
@@ -90,10 +90,10 @@ report_slope <- function(model, ..., ci = 0.95) {
   slopes$t <- round(slopes$t, 2)
 
   expr <- paste(
-    paste0("$t_{student}$ (", slopes$df_error, ") = ", slopes$t),
-    paste0("*p* ", format_p(slopes$p)),
     paste0("$\\beta$ = ", round(slopes$Coefficient, 2)),
     paste0("CI~",slopes$CI*100,"%~[", round(slopes$CI_low, 2), ", ", round(slopes$CI_high, 2), "]"),
+    paste0("$t_{student}$ (", slopes$df_error, ") = ", slopes$t),
+    paste0("*p* ", format_p(slopes$p)),
     sep = ", "
   )
 
@@ -109,12 +109,69 @@ report_slope <- function(model, ..., ci = 0.95) {
   return(expr)
 }
 
-#' Report smooth term from a GAM model
+#' Report linear slopes from a GAM model
+#'
+#' Report the linear combinations (i.e., slopes) for a GAM model fitted through effect derivatives.
+#'
+#' @param model Object of class "gam".
+#' @param term Character. Term present in the model for which estimate an effect
+#' @param ci Numeric. Confidence interval for coefficients. Defaults to 0.95.
+#' @param length Numeric. This arguments controls the number of (equally spread) values that will be taken to represent the continuous variable.
+#' @param k Numeric. Number of decimal places when printing numeric variables (Defaults to 1). Except for expression column.
+#' @param ... Further parameters passed to the `estimate_sloples()` from `modelbased` package.
+#'
+#' @export
+
+report_slopes <- function(model, term, ci = 0.95, length = 100, k = 1, ...) {
+
+  stopifnot(
+    "Is not a GAM model" = inherits(model, "gam"),
+    "Must specify a term for which estimate an effect" = !missing(term)
+  )
+
+  slopes <- modelbased::estimate_slopes(
+    model = model,
+    trend = term,
+    at = term,
+    ci = ci,
+    length = length,
+    ...
+  )
+
+  slopes <- summary(slopes)
+
+  slopes$df_error <- round(slopes$df_error, 2)
+  slopes$t <- round(slopes$t, 2)
+
+  expr <- paste(
+    paste0("$\\beta$ = ", round(slopes$Coefficient, 2)),
+    paste0("CI~",slopes$CI*100,"%~[", round(slopes$CI_low, 2), ", ", round(slopes$CI_high, 2), "]"),
+    paste0("$t_{student}$ (", slopes$df_error, ") = ", slopes$t),
+    paste0("*p* ", format_p(slopes$p)),
+    sep = ", "
+  )
+
+  if (nrow(slopes) > 1) {
+    col_ind <- colnames(slopes[seq.int(to = ncol(slopes) - 8)])
+    expr <- data.table::data.table(
+      slopes[col_ind],
+      expr,
+      key = col_ind
+    )
+    numeric_vars <- expr[, names(.SD), .SDcols = is.numeric]
+    expr[, (numeric_vars) := lapply(.SD, round, digits = 1), .SDcols = numeric_vars][]
+  }
+
+  return(expr)
+}
+
+#' Report smooth terms from a GAM model
 #'
 #' Get the text format of the statistics from the smooth term
 #' fitted from a GAM model fitted using the `mgcv` package.
 #'
 #' @param model Object of class "gam".
+#' @param ... Currently ignored.
 #'
 #' @export
 
@@ -124,23 +181,33 @@ report_smooth <- function(model, ...) {
     "Is not a GAM model" = inherits(model, "gam")
   )
 
+  mod_summary <- summary(model)
+
   tbl <- data.table::as.data.table(
-    x = summary(model)$s.table,
+    x = mod_summary$s.table,
     keep.rownames = TRUE
   )
 
-
-  tbl$df_error <- round(x = df.residual(model), digits = 2)
+  tbl$df_error  <- round(x = stats::df.residual(model), digits = 2)
   tbl$`p-value` <- format_p(tbl$`p-value`)
-  tbl$edf <- round(tbl$edf, 2)
-  tbl$F <- round(tbl$F, 2)
+  tbl$edf       <- round(tbl$edf, 2)
+  tbl$F         <- round(tbl$F, 2)
 
   expr <- paste(
     paste0("$F_{smooth}$ (", tbl$edf, ", ", tbl$df_error, ") = ", tbl$F),
     paste0("*p* ", tbl$`p-value`),
-    sep = ", "
+    sep = ", ",
+    recycle0 = TRUE
   )
 
+  if (length(tbl$rn) > 1) {
+    expr <- data.table::data.table(
+      term = tbl$rn,
+      expr,
+      key = "term"
+    )
+  }
 
+  return(expr)
 
 }
