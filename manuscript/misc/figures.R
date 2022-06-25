4d44073cb926b81d5c15923495e4fff881528012
Matías Castillo <matcasti@umag.cl>
2022-06-25 18:28:06 -0300

    Update figures
    - Removing fig 2 (now is part of fig 1 as a side panel)
    - Add `socio_individual` variable to plots (even though is not significant)

--- manuscript/misc/figures.R
+++ manuscript/misc/figures.R
@@ -8,7 +8,7 @@ source("manuscript/misc/setup.R")
 library(ggplot2)
 library(see)
 
-# Figure 1 ------------------------------------------------------------------------------------
+# Figure 1a ------------------------------------------------------------------------------------
 
 # Enviromental variable
 .save_plot <- TRUE
@@ -24,7 +24,7 @@ y_vars <- c("Communication" = "comunicacion_total"
             , "Fine motor" = "motora_fina_total"
             , "Gross motor" = "motora_gruesa_total"
             , "Problem solving" = "resolucion_problemas_total"
-          # , "Socio-individual" = "socio_individual_total"
+            , "Socio-individual" = "socio_individual_total"
           )
 
 fig_data <- melt(
@@ -37,31 +37,25 @@ fig_data[, variable := `levels<-`(variable, names(y_vars))][]
 scaled <- copy(fig_data)
 scaled[, edad_corregida_meses := ASQ3:::near(edad_corregida_meses, 5)][]
 
-fig_1 <- ggplot(fig_data, aes(x = edad_corregida_meses, y = value)) +
-  facet_wrap(~variable, ncol = 2) +
+fig_1a <- ggplot(fig_data, aes(x = edad_corregida_meses, y = value)) +
+  facet_grid(variable ~ ., scale = "free_y") +
   geom_count(alpha = 0.1, na.rm = TRUE) +
   stat_summary(data = scaled, fun.data = mean_se, na.rm = TRUE) +
   geom_smooth(method = "gam",
               formula = y ~ s(x),
-              col = "red4",
+              col = "red3",
               method.args = list(method = "REML"),
               na.rm = TRUE) +
-  labs(x = "Corrected age (months)", y = "Score",
-       caption = "Mean ± SE in 5-unit intervals") +
+  labs(x = "Corrected age (months)", y = "Score") +
   ggpubr::theme_pubr()
 
-if (isTRUE(x = .save_plot)) {
-  pdf("manuscript/fig-1.pdf", width = 7, height = 7);
-  print(fig_1);
-  dev.off()
-}
-
-# Figure 2 ------------------------------------------------------------------------------------
+# Figure 1b ------------------------------------------------------------------------------------
 
 models <- list("Communication" = NULL,
                "Gross motor" = NULL,
                "Fine motor" = NULL,
-               "Problem solving" = NULL)
+               "Problem solving" = NULL,
+               "Socio-individual" = NULL)
 
 models[[1]] <- gam(
   comunicacion_total ~
@@ -95,6 +89,14 @@ models[[4]] <- gam(
   method = "REML"
   )
 
+models[[5]] <- gam(
+  socio_individual_total ~
+    s(profesional_id, sexo_paciente, respondedor_vinculo, bs = "re") +
+    s(edad_corregida_meses),
+  data = dataset,
+  method = "REML"
+  )
+
 slopes <- sapply(
   X = models,
   FUN = modelbased::estimate_slopes,
@@ -104,21 +106,25 @@ slopes <- sapply(
   simplify = FALSE
 )
 
-fig2_data <- rbindlist(slopes, idcol = "response")
-fig2_data[, Confidence := fifelse(p < 0.05, "Significant", "Not significant")
+fig_1b_data <- rbindlist(slopes, idcol = "response")
+fig_1b_data[, Confidence := fifelse(p < 0.05, "Significant", "Not significant")
           ][, grp := rleid(Confidence)][]
 
-fig_2 <- ggplot(fig2_data, aes(edad_corregida_meses, Coefficient)) +
-  facet_wrap(~response) +
+fig_1b <- ggplot(fig_1b_data, aes(edad_corregida_meses, Coefficient)) +
+  facet_grid(response ~ ., scales = "free_y") +
   labs(x = "Corrected age (months)",
-       y = "Effect of Corrected age") +
+       y = "Effect of Corrected age",) +
   geom_hline(yintercept = 0, lty = 2) +
-  geom_line(aes(group = 1)) +
+  geom_line(aes(group = 1, col = Confidence), lwd = 1) +
   geom_ribbon(aes(ymin = CI_low, ymax = CI_high, group = grp, fill = Confidence), alpha = 0.4) +
-  ggpubr::theme_pubr()
+  ggpubr::theme_pubr() +
+  scale_color_manual(values = ggsci::pal_lancet()(2), aesthetics = c("fill", "col"))
+
+fig_1 <- ggpubr::ggarrange(fig_1a, fig_1b, ncol = 2)
 
 if (isTRUE(x = .save_plot)) {
-  pdf("manuscript/fig-2.pdf", width = 7, height = 7);
-  print(fig_2);
+  pdf("manuscript/fig-1.pdf", width = 12, height = 10);
+  print(fig_1);
   dev.off()
 }
+
