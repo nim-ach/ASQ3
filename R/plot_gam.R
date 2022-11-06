f1105b775f174cb6250d42b41d31564d04113234
Matías Castillo <matcasti@umag.cl>
2022-11-06 15:31:07 -0300

    add figure 3 and its code, and new function
    - New function to calculate logistic GAMs providing a numeric response and a threshold
    - Figure 3 and its code

--- R/plot_gam.R
+++ R/plot_gam.R
@@ -158,3 +158,101 @@ utils::globalVariables(
             "Adjusted for:"),
   add = TRUE
 )
+
+
+#' Plot ordinal GAM
+#'
+#' Display graphical representation of ordinal regression gam models
+#' on the response using the random effects structure and comparing
+#' the estimates.
+#'
+#' @param data Dataset from where to extract response and independent variables.
+#' @param var Character. Response variable to test on gam models with varying random effects structures.
+#' @param seed Numeric. Seed for random processes within gams.
+#' @param legend Logical. Whether to include the figure legend.
+#' @param var_name Character. The label for the y-axis. Default is NULL, so "Predicted score" is used.
+#' @param plot Logical. Whether to return the plot. If FALSE, then the models with the varying random effects structures are returned.
+#' @param threshold Numeric. Threshold to calculate probability.
+#' @param ... Currently not used.
+#'
+#' @export
+
+gam_ordinal <- function(data, var, seed = 1234, legend = TRUE, var_name = NULL, plot = TRUE, threshold = 35, ...) {
+  model <- "as.numeric(%var% < threshold) ~ s(profesional_id, bs = \"re\") + s(sexo_paciente, bs = \"re\") + s(respondedor_vinculo, bs = \"re\") + s(edad_corregida_meses)"
+  model <- gsub("%var%", replacement = var, model)
+  model <- stats::as.formula(model)
+
+  set.seed(seed)
+  output <- mgcv::gam(model, family = stats::binomial(link = "logit"), data = data)
+
+  if (isFALSE(plot)) {
+    return(output)
+  }
+
+  testdata <- expand.grid(
+    profesional_id = c(2),
+    sexo_paciente = c("F"),
+    respondedor_vinculo = c("Madre"),
+    edad_corregida_meses = seq(0, 48, 0.05)
+  )
+
+  predicted_response <- as.data.table(x = stats::predict(output, testdata, se.fit = TRUE, type = "response"))
+  predicted_response <- cbind(predicted_response, testdata)
+
+  names(predicted_response)[1:2] <- c("Fit", "SE")
+
+  ylab <- "P(Category | Corrected Age)"
+  if (!is.null(var_name) && is.character(var_name)) {
+    ylab <- paste0("P(", paste(var_name, "<", threshold), " | Corrected Age)")
+  }
+
+  ci_prob <- function(p) stats::qnorm((p+1)/2, lower.tail = TRUE)
+
+  plot <- ggplot2::ggplot(predicted_response, ggplot2::aes(edad_corregida_meses, Fit)) +
+    ggplot2::geom_ribbon(
+      mapping = ggplot2::aes(
+        ymin = ifelse((Fit - SE * ci_prob(p = .95)) < 0, 0, (Fit - SE * ci_prob(p = .95))),
+        ymax = ifelse(Fit + SE * ci_prob(p = .95) > 1, 1, Fit + SE * ci_prob(p = .95)),
+        fill = "95%"
+      )
+    ) +
+    ggplot2::geom_ribbon(
+      ggplot2::aes(
+        ymin = ifelse((Fit - SE * ci_prob(p = .80)) < 0, 0, (Fit - SE * ci_prob(p = .80))),
+        ymax = ifelse(Fit + SE * ci_prob(p = .80) > 1, 1, Fit + SE * ci_prob(p = .80)),
+        fill = "80%"
+      )
+    ) +
+    ggplot2::geom_ribbon(
+      ggplot2::aes(
+        ymin = ifelse(Fit - SE * ci_prob(p = .5) < 0, 0, Fit - SE * ci_prob(p = .5)),
+        ymax = ifelse(Fit + SE * ci_prob(p = .5) > 1, 1, Fit + SE * ci_prob(p = .5)),
+        fill = "50%"
+      )
+    ) +
+    ggplot2::geom_line(col = "white") +
+    ggplot2::scale_fill_manual(values = c("95%" = "#FEE0D2", "80%" = "#FC9272", "50%" = "#DE2D26"), aesthetics = c("col", "fill")) +
+    ggdist::theme_ggdist() +
+    ggplot2::scale_y_continuous(expand = c(0,0), limits = c(0,1)) +
+    ggplot2::scale_x_continuous(expand = c(0,0)) +
+    ggplot2::labs(x = "Corrected age (in months)", y = ylab, fill = "CI:", col = "CI:") +
+    ggplot2::theme(legend.position = "top")
+
+  if (isFALSE(legend)) {
+    plot <- plot + ggplot2::theme(legend.position = "none")
+  }
+
+  return(plot)
+}
+
+# Global variables
+utils::globalVariables(
+  names = c("edad_corregida_meses",
+            "respondedor_vinculo",
+            "sexo_paciente",
+            "fit", "Fit",
+            "SE",
+            "variable",
+            "Adjusted for:"),
+  add = TRUE
+)
--- manuscript/figures/fig-3.R
+++ manuscript/figures/fig-3.R
@@ -3,9 +3,48 @@
 ## Script de preparación
 source("manuscript/misc/setup.R")
 
+## Cargamos paquetes
+library(ggplot2)
+
 # Preparamos los datos ----------------------------------------------------
 
-ind <- grep("interpretacion", names(dataset), value = TRUE)
+ind <- grep("total", names(dataset), value = TRUE)
+names(ind) <- c("CM", "GM", "FM", "CG", "PS")
+
+plots <- mapply(
+  FUN = gam_ordinal,
+  data = list(dataset),
+  var = ind,
+  var_name = names(ind),
+  legend = c(T,T,F,F,F),
+  threshold = 35,
+  SIMPLIFY = FALSE
+)
+
+f0 <- ggpubr::ggarrange(plotlist = plots[1:2], nrow = 1, common.legend = TRUE)
+f1 <- ggpubr::ggarrange(plotlist = plots[3], nrow = 1)
+f2 <- ggpubr::ggarrange(plotlist = plots[4:5], nrow = 1)
+figure3 <- ggpubr::ggarrange(f0, f1, f2, ncol = 1)
+
+local({
+  pdf("manuscript/figures/fig-3.pdf", width = 10, height = 12);
+  print(figure3);
+  dev.off()
+})
+
+
+# Evaluacion de los modelos -----------------------------------------------
+
+models <- mapply(
+  FUN = gam_ordinal,
+  var = ind,
+  data = list(dataset),
+  plot = FALSE,
+  threshold = 35,
+  SIMPLIFY = FALSE,
+  USE.NAMES = TRUE
+)
 
 
 
+lapply(models, report_overall)
