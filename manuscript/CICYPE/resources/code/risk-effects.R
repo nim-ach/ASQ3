b195111d7cc78127bb58b60dc97ef0f9b41b92c7
Matías Castillo <matcasti@umag.cl>
2022-12-15 02:02:44 -0300

    update presentation based on CN suggestions

--- manuscript/CICYPE/resources/code/risk-effects.R
+++ manuscript/CICYPE/resources/code/risk-effects.R
@@ -98,19 +98,7 @@ plot_risk_effects <- function(var, var_name = var, save = FALSE) {
   }
 }
 
-for (i in names(ind[1:2])) {
+for (i in names(ind)) {
   plot_risk_effects(i, save = TRUE)
 }
-
-fig <- ggpubr::ggarrange(
-  plotlist = lapply(names(ind[3:5]), plot_risk_effects),
-  nrow = 1,
-  ncol = 3,
-  common.legend = TRUE
-)
-svglite::svglite(filename = "manuscript/CICYPE/resources/results_risk_NS.svg",
-                 height = 5, width = 10,
-                 always_valid = TRUE)
-print(fig)
-dev.off()
 
